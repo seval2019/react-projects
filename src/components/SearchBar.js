5226112ae16248a659d4f3cf41ebbf0af0da7174
Seval Senturk <seval.senturk@tgs.aero>
2023-05-22 08:56:02 +0300

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+    render(){
+        return(
+                <h3>Search</h3>
+        )
+    }
+}
+
+export default SearchBar;
